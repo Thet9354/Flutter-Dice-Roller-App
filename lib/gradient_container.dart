5e3a49afe616c9fee2796e9763f50d02f3fd3654
PhoonThetPine <thetpine254@gmail.com>
2023-05-22 10:45:30 +0800

    Flutter Dice Roller App - V1

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({Key? key, required this.colors}) : super(key: key);
+
+  final List<Color> colors;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colors, begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
